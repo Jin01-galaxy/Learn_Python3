d7411cfbe6212c0ce2ba8bc26ad5bde578f533b4
Jin01-galaxy <63948777+Jin01-galaxy@users.noreply.github.com>
2020-09-17 15:29:07 +0900

    commit

--- Python_Basic/class.py
+++ Python_Basic/class.py
@@ -1,15 +1,18 @@
 class Planet:
-  def __init__(self):
-    self.name = 'Mars'
-    self.radius = 200000
-    self.gravity = 2.1
-    self.system = 'Solar System'
-
+  def __init__(self,name,radius,gravity,system):
+    self.name = name
+    self.radius = radius
+    self.gravity = gravity
+    self.system = system
   def orbit(self):
     return f'{self.name} is orbiting in the {self.system}'
 
 
-mars = Planet()
+mars = Planet('Mars',200000,5.5,'Solar System')
 print(f'Name is: {mars.name}')
 print(mars.orbit())
 
+naboo= Planet('Naboo',300000,8,'Naboo System')
+print(f'Name is: {naboo.name}')
+print(naboo.gravity)
+print(naboo.orbit())
