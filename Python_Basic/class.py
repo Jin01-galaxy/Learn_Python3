09b69f78c4f9ad8236bc22f725ed836bc9e035ea
Jin01-galaxy <63948777+Jin01-galaxy@users.noreply.github.com>
2020-09-18 21:34:57 +0900

    @staticmethod

--- Python_Basic/class.py
+++ Python_Basic/class.py
@@ -1,4 +1,5 @@
 class Planet:
+  shape = 'round'
   def __init__(self,name,radius,gravity,system):
     self.name = name
     self.radius = radius
@@ -7,12 +8,23 @@ class Planet:
   def orbit(self):
     return f'{self.name} is orbiting in the {self.system}'
 
+  @classmethod
+  def commons(cls):
+    return f'All planets are {cls.shape} because of gravity.'
+
 
 mars = Planet('Mars',200000,5.5,'Solar System')
 print(f'Name is: {mars.name}')
 print(mars.orbit())
 
-naboo= Planet('Naboo',300000,8,'Naboo System')
+naboo = Planet('Naboo',300000,8,'Naboo System')
 print(f'Name is: {naboo.name}')
 print(naboo.gravity)
+
+
+@staticmethod
+def spin(speed= '2000 miles per hour'):
+  return f'spins {speed}'
+
+# print(Planet.shape)
 print(naboo.orbit())
